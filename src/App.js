a4ca283044160ba8bf8a614dce1c1ec145b7e788
Morenibose <109108462+Morenibose@users.noreply.github.com>
2022-11-22 18:35:48 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/morenike-adebayo-7b0a5151/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Morenike Adebayo
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Morenibose/react-weather-app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
