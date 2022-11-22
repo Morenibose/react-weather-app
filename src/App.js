fea84db0f8bd92584a71c430385138a2ec19f7b6
Morenibose <109108462+Morenibose@users.noreply.github.com>
2022-11-22 20:29:03 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/morenike-adebayo-7b0a5151/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Morenike Adebayo
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Morenibose/react-weather-app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/morenike-adebayo-7b0a5151/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Morenike Adebayo
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Morenibose/react-weather-app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
