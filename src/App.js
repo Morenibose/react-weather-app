2726daa8b138a7d31af514f8ba7d7e2325395d00
Morenibose <109108462+Morenibose@users.noreply.github.com>
2022-11-28 05:59:47 +0100

    Updated the footer with Netlify Url

--- src/App.js
+++ src/App.js
@@ -23,7 +23,16 @@ export default function App() {
             target="_blank"
             rel="noreferrer"
           >
-            open-sourced on GitHub
+            open-sourced on GitHub,
+          </a>
+          and
+          <a
+            href="https://leafy-taffy-db3794.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            hosted on Netlify
           </a>
         </footer>
       </div>
